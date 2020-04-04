ff03b589e7bcea8b8f5797dc2bb46bdfbf8c40c9
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-04-04 14:24:45 +0100

    Use pkgconfig module to find libiscsi when building the extension.
    This allows building on Fedora and other distributions that might put the
    library in different paths, as well as reporting a proper error if a
    too-old version of the library is installed.

--- setup.py
+++ setup.py
@@ -5,20 +5,28 @@
 
 from setuptools import Extension, find_packages, setup
 
-# Ensure it's present.
-import setuptools_scm  # noqa: F401
+import pkgconfig
+import setuptools_scm  # noqa: F401  # Ensure it's present.
 from Cython.Build import cythonize
+
+if not pkgconfig.installed("libiscsi", ">=1.13"):
+    raise Exception(
+        "libiscsi 1.13 not found, make sure you installed libiscsi-dev package, or equivalent"
+    )
+
+libiscsi_pkg = pkgconfig.parse("libiscsi")
 
 setup(
     packages=find_packages(),
     package_data={"": ["*.pyx", "*.pxd"]},
     ext_modules=cythonize(
-        Extension(name="iscsi", sources=["src/iscsi.pyx"], libraries=["iscsi"])
+        Extension(name="iscsi", sources=["src/iscsi.pyx"], **libiscsi_pkg)
     ),
     extras_require={
         "dev": [
             "Cython",
             "mypy",
+            "pkgconfig",
             "pre-commit",
             "pytest",
             "setuptools>=42",
