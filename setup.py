ae8209adc29900496201b89c4c5547d853c9f2f2
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-05-16 16:54:33 +0100

    Move more parameters of setup() to the declarative setup.cfg.
    This simplifies the setup.py logic quite a bit, as it's only there for the extension building.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
-from setuptools import Extension, find_packages, setup
+from setuptools import Extension, setup
 
 import pkgconfig
 import setuptools_scm  # noqa: F401  # Ensure it's present.
@@ -16,21 +16,7 @@ if not pkgconfig.installed("libiscsi", ">=1.13"):
 libiscsi_pkg = pkgconfig.parse("libiscsi")
 
 setup(
-    packages=find_packages(),
-    package_data={"": ["*.pyx", "*.pxd"]},
     ext_modules=cythonize(
         Extension(name="iscsi", sources=["src/iscsi.pyx"], **libiscsi_pkg)
     ),
-    extras_require={
-        "dev": [
-            "Cython",
-            "mypy",
-            "pkgconfig",
-            "pre-commit",
-            "pytest",
-            "setuptools>=42",
-            "setuptools_scm[toml]>=3.4",
-            "wheel",
-        ]
-    },
 )
