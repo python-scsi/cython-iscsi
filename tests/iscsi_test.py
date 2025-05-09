74a7f80657f7eec7f3c2d1c12009ec0c105c5d40
Brian M <brian.meagher@ixsystems.com>
2025-05-09 11:56:45 -0700

    Fix tests for modern cython

--- tests/iscsi_test.py
+++ tests/iscsi_test.py
@@ -17,26 +17,32 @@ class ContextTest(unittest.TestCase):
     def test_set_context_params(self):
         context = iscsi.Context("foobar")
         context.set_targetname("my-target")
-        context.set_header_digest(iscsi.ISCSI_HEADER_DIGEST_NONE)
+        context.set_header_digest(iscsi.iscsi_header_digest.ISCSI_HEADER_DIGEST_NONE)
 
     def test_command(self):
         context = iscsi.Context("foobar")
-        task = iscsi.Task(b"\x12\x00\x00\x00\x60\x00", iscsi.SCSI_XFER_READ, 96)
+        task = iscsi.Task(
+            b"\x12\x00\x00\x00\x60\x00", iscsi.scsi_xfer_dir.SCSI_XFER_READ, 96
+        )
         data_in = bytearray(96)
         context.command(0, task, None, data_in)
 
 
 class TaskTest(unittest.TestCase):
     def test_basic(self):
-        task = iscsi.Task(b"\x12\x00\x00\x00\x60\x00", iscsi.SCSI_XFER_READ, 96)
+        task = iscsi.Task(
+            b"\x12\x00\x00\x00\x60\x00", iscsi.scsi_xfer_dir.SCSI_XFER_READ, 96
+        )
         self.assertIsNotNone(task)
 
     def test_bytearray(self):
         task = iscsi.Task(
-            bytearray(b"\x12\x00\x00\x00\x60\x00"), iscsi.SCSI_XFER_READ, 96
+            bytearray(b"\x12\x00\x00\x00\x60\x00"),
+            iscsi.scsi_xfer_dir.SCSI_XFER_READ,
+            96,
         )
         self.assertIsNotNone(task)
 
     def test_empty_cdb(self):
         with self.assertRaises(ValueError):
-            iscsi.Task(None, iscsi.SCSI_XFER_READ, 96)
+            iscsi.Task(None, iscsi.scsi_xfer_dir.SCSI_XFER_READ, 96)
